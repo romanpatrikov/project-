c24328498c4c1bbbe848ed3d9519dd8ba658ba41
Roman <romanpatrickov@gmail.com>
2019-11-10 17:54:40 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+"use strict"
+a = 5 ;
+console.log(a)
+// // var LeftBorderWidth = 1;
+// // {
+    
+// // let second = 2;
+// // console.log(second);
+// // }
+// // const pi = 3;
+// // console.log(LeftBorderWidth);
+
+// // {
+// //     var pushka = 1;
+    
+// // }
+
+// // {
+// //     let pullya =2;
+// //     console.log(pullya);
+// //     console.log(pushka);
+// // }
+ 
+//  var obj = {
+//  name : "john",
+//  age : 25,
+//  sinlg : false 
+//  };
+//  console.log(obj.age);
